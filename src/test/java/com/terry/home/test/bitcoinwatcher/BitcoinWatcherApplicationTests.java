938d5a21e9655ad7251fef4591f35282b8c7931d
jhaoweihuang <jhaowei.huang@gmail.com>
2020-03-10 09:42:23 +0800

    新增編譯完的靜態檔案

---
+++ src/test/java/com/terry/home/test/bitcoinwatcher/BitcoinWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terry.home.test.bitcoinwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
